6574e4bdc3ea8c41316d5254fe0a6887b5035086
Daniel Costache <ccristian2001@gmail.com>
2024-01-12 12:19:54 +0200

    initial commit

---
+++ build/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/daniel/Code/geant4/sim/sim.cc" "CMakeFiles/sim.dir/sim.cc.o" "gcc" "CMakeFiles/sim.dir/sim.cc.o.d"
+  "/home/daniel/Code/geant4/sim/src/action.cc" "CMakeFiles/sim.dir/src/action.cc.o" "gcc" "CMakeFiles/sim.dir/src/action.cc.o.d"
+  "/home/daniel/Code/geant4/sim/src/construction.cc" "CMakeFiles/sim.dir/src/construction.cc.o" "gcc" "CMakeFiles/sim.dir/src/construction.cc.o.d"
+  "/home/daniel/Code/geant4/sim/src/detector.cc" "CMakeFiles/sim.dir/src/detector.cc.o" "gcc" "CMakeFiles/sim.dir/src/detector.cc.o.d"
+  "/home/daniel/Code/geant4/sim/src/generator.cc" "CMakeFiles/sim.dir/src/generator.cc.o" "gcc" "CMakeFiles/sim.dir/src/generator.cc.o.d"
+  "/home/daniel/Code/geant4/sim/src/physics.cc" "CMakeFiles/sim.dir/src/physics.cc.o" "gcc" "CMakeFiles/sim.dir/src/physics.cc.o.d"
+  "/home/daniel/Code/geant4/sim/src/run.cc" "CMakeFiles/sim.dir/src/run.cc.o" "gcc" "CMakeFiles/sim.dir/src/run.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
